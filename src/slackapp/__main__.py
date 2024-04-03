4a10a4e1c3db744927fb0c0f2b0ed3608c7539f9
Ion <iontapordei@gmail.com>
2024-04-03 19:19:59 +0000

    wip - connect and test

--- src/slackapp/__main__.py
+++ src/slackapp/__main__.py
@@ -25,6 +25,33 @@ def utils() -> None:
     pass
 
 
+@utils.command()
+def refresh_configuration_token() -> None:
+    """
+    The token expires after 12 hours, and has to be refreshed. To achieve this,
+    we need the refresh token, which is also provided by Slack. This command
+    refreshes the configuration token and updates the environment variables.
+    
+    See:
+    - https://api.slack.com/apps
+    - https://api.slack.com/authentication/token-types#config
+    """
+    # TODO: Set this up in a cron job, in app's dedicated container
+    LOG.info("Refreshing the configuration token")
+    response = APP.client.tooling_tokens_rotate(
+        refresh_token=os.environ["SLACK_REFRESH_TOKEN"]
+    )
+
+    if not response.data["ok"]:
+        LOG.error(f"Failed to refresh the configuration token: {response.data}")
+        return
+
+    os.environ["SLACK_CONFIGURATION_TOKEN"] = response.data["token"]
+    os.environ["SLACK_REFRESH_TOKEN"] = response.data["refresh_token"]
+
+    # TODO: Hit a starship endpoint to update config and refresh tokens
+
+
 app.add_command(utils)
 
 
--- src/slackapp/start.py
+++ src/slackapp/start.py
@@ -1,37 +1,58 @@
 import os
+import logging
+
 from slack_bolt import App
 from slack_bolt.oauth.oauth_settings import OAuthSettings
 from slack_sdk.oauth.installation_store import FileInstallationStore
 from slack_sdk.oauth.state_store import FileOAuthStateStore
 
 
-oauth_settings = OAuthSettings(
-    client_id=os.environ["SLACK_CLIENT_ID"],
-    client_secret=os.environ["SLACK_CLIENT_SECRET"],
-    scopes=["channels:read", "groups:read", "chat:write"],
-    installation_store=FileInstallationStore(base_dir="./data/installations"),
-    state_store=FileOAuthStateStore(expiration_seconds=600, base_dir="./data/states"),
-)
+# oauth_settings = OAuthSettings(
+#     client_id=os.environ["SLACK_CLIENT_ID"],
+#     client_secret=os.environ["SLACK_CLIENT_SECRET"],
+#     scopes=["chat:write", "commands"],
+#     installation_store=FileInstallationStore(base_dir="./data/installations"),
+#     state_store=FileOAuthStateStore(expiration_seconds=600, base_dir="./data/states"),
+# )
 
-APP = App(
-    signing_secret=os.environ["SLACK_SIGNIN_SECRET"], oauth_settings=oauth_settings
-)
+# APP = App(
+#     signing_secret=os.environ["SLACK_SIGNIN_SECRET"], oauth_settings=oauth_settings
+# )
+APP = App(token="xoxb-6800962808624-6929532365808-6MKUetgV4JR0hE7xcmkJuAUc")
+LOG = logging.getLogger("slackapp")
 
 
 @APP.event("app_home_opened")
-def update_home_tab(client: App, event: dict):
-    client.views_publish(
-        user_id=event["user"],
-        view={
-            "type": "home",
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {"type": "mrkdwn", "text": "Welcome home!"},
-                },
-            ],
-        },
-    )
+def update_home_tab(client, event, logger):
+    LOG.info("Updating home tab")
+    try:
+        # Call views.publish with the built-in client
+        client.views_publish(
+            # Use the user ID associated with the event
+            user_id=event["user"],
+            # Home tabs must be enabled in your app configuration
+            view={
+                "type": "home",
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "*Wilkomen home, <@" + event["user"] + "> :house:*",
+                        },
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>.",
+                        },
+                    },
+                ],
+            },
+        )
+    except Exception as e:
+        LOG.error(f"Error publishing home tab: {e}")
 
 
 @APP.command("/hello-bolt-python")
@@ -41,5 +62,13 @@ def hello_command(ack, body):
 
 @APP.message(":wave:")
 def say_hello(message, say):
+    LOG.info("Saying hello")
+    user = message["user"]
+    say(f":wave: <@{user}>!")
+
+
+@APP.message("hello")
+def say_hello2(message, say):
+    LOG.info("Saying hello2")
     user = message["user"]
     say(f"Hi there, <@{user}>!")
