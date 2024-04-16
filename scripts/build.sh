b2d23cd1d0a995af64ba7f2e12627a288d081b20
Ion <iontapordei@gmail.com>
2024-04-16 09:54:15 +0000

    add build script and spec

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+APP="firedust-slack"
+
+# Set env variables
+IMAGE=$(aws ecr describe-repositories --repository-names "$APP" --query 'repositories[0].repositoryUri' --output text)
+
+# Retrieve spells
+docker_wizard=$(aws secretsmanager get-secret-value --secret-id docker/wizard --query SecretString --output text)
+docker_spell=$(aws secretsmanager get-secret-value --secret-id docker/spell --query SecretString --output text)
+
+# Login to docker
+docker login -u="${docker_wizard}" -p="${docker_spell}"
+
+# Build and push docker image
+docker build --target "$APP"-latest . -t "$IMAGE":latest
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin "$IMAGE"
+docker push "$IMAGE":latest
